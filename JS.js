1d78acbe2226fb95dc882fe76c8783eacdc7f64d
Catalina Laiton <lclaitonp@udistrital.edu.co>
2024-04-05 12:14:39 -0500

    pdf

--- JS.js
+++ JS.js
@@ -177,3 +177,36 @@ const escenariosOcupacionales = [
 
   }
   
+  document.addEventListener("DOMContentLoaded", () => {
+    const $boton = document.querySelector("#btnCrearPdf");
+    $boton.addEventListener("click", (event) => {
+        event.preventDefault(); // Detiene el comportamiento predeterminado del formulario
+
+        const $elementoParaConvertir = document.body;
+        html2pdf()
+            .set({
+                margin: 1,
+                filename: 'RES_773.pdf',
+                image: {
+                    type: 'jpeg',
+                    quality: 0.98
+                },
+                html2canvas: {
+                    scale: 3,
+                    letterRendering: true,
+                },
+                jsPDF: {
+                    unit: "in",
+                    format: "a3",
+                    orientation: 'portrait'
+                }
+            })
+            .from($elementoParaConvertir)
+            .save()
+            .catch(err => console.log(err))
+            .finally()
+            .then(() => {
+                console.log("Guardado!")
+            })
+    })
+})
